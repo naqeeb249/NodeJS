23ceb8f1cd7e44455b58eeb1203bc6752966f893
ADMIN <naqeeb249@gmail.com>
2021-05-27 12:48:09 +0530

    Read from file and show it on home added

--- route.js
+++ route.js
@@ -7,14 +7,22 @@ const requestHandler = (req, res) => {
     const url = req.url;
     const method = req.method;
     
-
     if(url === '/'){
-        res.write('<html>')
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action = "/message"  method = "POST"><input type = "text" name="message"><button type ="submit">Send</button></form></body>')
-         res.write('</html>')
-         return res.end(); 
+      
+      fs.readFile('message.txt', (err,file) => {
+         const data = file.toString();
+
+         res.statusCode = 200;
+         res.write('<html>')
+         res.write('<head><title>Enter Message</title></head>');
+            res.write(`<body><div>${data}<form  action = "/message"  method = "POST"><input id = "send" type = "text" name="title"><button onclick ="document.getElementById('send').value += '-' window.localStorage.getItem('username')" type ="submit">Send</button></form></div></body>`)
+            res.write('</html>')
+             res.end();  
+          })
+      return
+               
     }
+
     if(url === '/message' && method === 'POST'){
     const body = [];
      req.on('data', (chunk) => {
@@ -22,43 +30,18 @@ const requestHandler = (req, res) => {
      })
     
     
-    req.on('data', () => {
+   return req.on('end', () => {
          const bodyParsed = Buffer.concat(body).toString();
          const message = `${bodyParsed.split('=')[1]}`;
     
-        fs.writeFile('message.txt', message, (err) =>{
-          /* res.statusCode = 302;
-          res.setHeader('Location', '/'); */
+        fs.appendFile('message.txt', message, (err) =>{
+         res.statusCode = 301;
+         res.setHeader('Location','/');
+        return res.end();
                        
      })
      })
-    return  req.on('end', () => {
-      
-        fs.readFile('message.txt', (err,file) => {
-            
-            const buf = Buffer.from(file);
-            const readData = buf.toString();
-          
-            
-           
-             res.writeHead(301, {
-              'Location': '/',
-              'Content-Type': 'text/plain'
-              
-          }); 
-          
-       
-          /*   res.write('<html>')
-            res.write('<head><title>Enter Message</title></head>');
-            res.write(`<html><h1>${readData}</h1>`);
-            res.write('<body><form action = "/message"  method = "POST"><input type = "text" name="message"><button type ="submit">Send</button></form></body>')
-             res.write('</html>') */
-        
-       console.log(readData);
-        return res.end(readData);
-            
-        })
-    })
+
     
     
     }
@@ -66,7 +49,7 @@ const requestHandler = (req, res) => {
     if(url === '/login'){
     
     
-      res.write(`<html><form onsubmit="localStorage.setItem('username', document.getElementById('username').value)" action="/message" method="POST">
+      res.write(`<html><form onsubmit="localStorage.setItem('username', document.getElementById('username').value)" action="/product" method="POST">
     
         <input id="username" type="text" name="title">
     
@@ -79,19 +62,11 @@ const requestHandler = (req, res) => {
     
     if(url === '/product' && method === 'POST')
     {
-    const body = [];
-      req.on('data',(chunk) => {
-      body.push(chunk);
-      })
     
-      return req.on('end',() => {
-        const bodyParsed = Buffer.concat(body).toString();
-        const username = bodyParsed.split('=')[1];
-        console.log(username);
-        res.statusCode = 302;
+        res.statusCode = 301;
         res.setHeader('Location','/');
-        return res.end();
-      })
+        res.end();
+        return 
      
     }
     
