be803997ea64759a5f412d11e88104163b62fd9c
ADMIN <naqeeb249@gmail.com>
2021-05-27 16:54:39 +0530

    Midleware and bodyParser

--- ExpressJs/app.js
+++ ExpressJs/app.js
@@ -1,14 +1,23 @@
+
 const express = require('express');
+
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next) => {
-    console.log('In the middleware');
-    next();
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use('/get-product', (req,res) => {
+    res.send('<form action = "/product" method = "POST"><input type = "text" name = "title"><input type = "number" name="size"><button>Send</button></form>');
+})
+
+app.post('/product', (req,res) => {
+    console.log(req.body);
+    res.redirect('/');
 })
 
 app.use((req,res,next) => {
-    console.log('In another middleware');
+    
     res.send('<h1>Hello form Express</h1>');
     
 })
