a9e771791ea68533a2249d72bd9cf38fb7857d48
ADMIN <naqeeb249@gmail.com>
2021-06-05 11:50:08 +0530

    Design product and user relationship

--- ExpressJs/Express3/controllers/admin.js
+++ ExpressJs/Express3/controllers/admin.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const User = require('../models/user');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -14,12 +15,13 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   
-  Product.create({
+  req.user.createProduct({
     title : title,
     price : price,
     imageUrl : imageUrl,
     description : description
-  }).then(result => {
+  })
+.then(result => {
     console.log('Product added successfully');
     res.redirect('/admin/products');
   })
@@ -32,7 +34,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId).then(product => {
+ req.user.getProducts({where : {id : prodId}})
+  .then(products => {
+    const product = products[0];
     if (!product) {
       return res.redirect('/');
     }
@@ -69,7 +73,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
   .then(products => {
     res.render('admin/products', {
       prods: products,
